edc523417101476ed66ed1f73563bf10a9793f53
Andrei Grini <Andrei.Grini@avanssoma.no>
2021-04-06 17:21:27 +0300

    initial commit

---
+++ strings.js
@@ -0,0 +1,5 @@
+const string =
+    "JavaScript is the best language in the world. Lets learn JavaScript together"
+
+console.log(string.replace('JavaScript', 'laravel'))
+console.log(string.replaceAll('JavaScript', 'php'))
